01c07bf211e5bf152ef4b52a40aca3e399bc2ecf
Cristian Santiz <crisantizan@gmail.com>
2020-04-13 17:56:17 -0500

    style: define dark theme

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [
-    CommonModule,
-    LoginRoutingModule
-  ]
+  imports: [CommonModule, LoginRoutingModule, MaterialModule],
 })
-export class LoginModule { }
+export class LoginModule {}
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatFormFieldModule],
-  exports: [MatFormFieldModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+  ],
+  exports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatCardModule,
+    MatInputModule,
+  ],
 })
 export class MaterialModule {}
