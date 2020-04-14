2d1109baba4927015a0efec4d9f2563ebf1b69a8
Cristian Santiz <crisantizan@gmail.com>
2020-04-14 17:16:46 -0500

    feat: add password field component

--- src/app/dialogs/dialogs.module.ts
+++ src/app/dialogs/dialogs.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { UserRegisterComponent } from './user-register/user-register.component';
 import { MaterialModule } from '../material.module';
 import { DirectivesModule } from '../common/directives/directives.module';
+import { PasswordFieldComponent } from '../common/components/password-field.component';
 
 @NgModule({
-  declarations: [UserRegisterComponent],
+  declarations: [UserRegisterComponent, PasswordFieldComponent],
   imports: [CommonModule, MaterialModule, DirectivesModule],
 })
 export class DialogsModule {}
