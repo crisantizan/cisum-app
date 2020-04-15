6fbf8e55613ed0b4c2baef97e0d059039b3dc9c5
Cristian Santiz <crisantizan@gmail.com>
2020-04-14 20:22:44 -0500

    feat: reactive form in user register

--- src/app/dialogs/dialogs.module.ts
+++ src/app/dialogs/dialogs.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 import { UserRegisterComponent } from './user-register/user-register.component';
 import { MaterialModule } from '../material.module';
 import { DirectivesModule } from '../common/directives/directives.module';
-import { PasswordFieldComponent } from '../common/components/password-field.component';
 
 @NgModule({
-  declarations: [UserRegisterComponent, PasswordFieldComponent],
+  declarations: [UserRegisterComponent],
   imports: [CommonModule, MaterialModule, DirectivesModule],
 })
 export class DialogsModule {}
--- src/app/dialogs/user-register/user-register.component.ts
+++ src/app/dialogs/user-register/user-register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
@@ -7,13 +8,27 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./user-register.component.scss'],
 })
 export class UserRegisterComponent implements OnInit {
-  public hidePass: boolean = true;
+  public hidePass = { new: true, verify: true };
+  public form: FormGroup;
 
-  constructor(public dialogRef: MatDialogRef<UserRegisterComponent>) {}
+  constructor(
+    public dialogRef: MatDialogRef<UserRegisterComponent>,
+    public fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      verifyPassword: ['', Validators.required],
+    });
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form.valueChanges.subscribe((v) => console.log(v));
+  }
 }
