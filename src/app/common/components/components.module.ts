4f53673c5d04b640cf10133818a9db5285521e5e
Cristian Santiz <crisantizan@gmail.com>
2020-04-16 17:24:43 -0500

    feat: add audio-player component

--- src/app/common/components/components.module.ts
+++ src/app/common/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SongBoxComponent } from './song-box/song-box.component';
 import { MaterialModule } from 'src/app/material.module';
+import { AudioPlayerComponent } from './audio-player/audio-player.component';
 
 @NgModule({
-  declarations: [SongBoxComponent],
+  declarations: [SongBoxComponent, AudioPlayerComponent],
   imports: [CommonModule, MaterialModule],
-  exports: [SongBoxComponent],
+  exports: [SongBoxComponent, AudioPlayerComponent],
 })
 export class ComponentsModule {}
