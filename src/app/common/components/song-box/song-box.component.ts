6fb54d490441229cf5e365b1b22a1dcef570850d
Cristian Santiz <crisantizan@gmail.com>
2020-04-16 12:44:01 -0500

    style: animations in song-box component

--- src/app/common/components/song-box/song-box.component.ts
+++ src/app/common/components/song-box/song-box.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-song-box',
   templateUrl: './song-box.component.html',
-  styleUrls: ['./song-box.component.scss']
+  styleUrls: ['./song-box.component.scss'],
 })
 export class SongBoxComponent implements OnInit {
+  public showBackdrop: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public mouseEnter() {
+    this.showBackdrop = true;
   }
 
+  public mouseLeave() {
+    this.showBackdrop = false;
+  }
 }
