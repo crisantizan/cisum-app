8066aae50dbae9f60f13bcc0c84857bef4cecbf6
Cristian Santiz <crisantizan@gmail.com>
2020-04-16 23:37:05 -0500

    style: improvement in responsive

--- src/app/common/components/audio-player/audio-player.component.ts
+++ src/app/common/components/audio-player/audio-player.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-audio-player',
   templateUrl: './audio-player.component.html',
   styleUrls: ['./audio-player.component.scss'],
 })
-export class AudioPlayerComponent implements OnInit {
+export class AudioPlayerComponent implements OnInit, OnDestroy {
   public playerIcon: 'play_arrow' | 'pause' = 'play_arrow';
   public volumeIcon: 'volume_up' | 'volume_off' = 'volume_up';
-  public resizeIcon: 'expand_more' | 'expand_less' = 'expand_less';
+  // public resizeIcon: 'expand_more' | 'expand_less' = 'expand_less';
+  public expanded: boolean = false;
+  public xsDevice: boolean;
 
-  constructor() {}
+  constructor(private breakpoint: BreakpointObserver) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const xs = this.breakpoint.observe('(max-width: 350px)');
+
+    xs.subscribe((obs) => {
+      console.log(obs);
+      this.xsDevice = obs.matches;
+    });
+  }
+
+  public onResize() {
+    this.expanded = !this.expanded;
+  }
+
+  ngOnDestroy() {
+    this.breakpoint.ngOnDestroy();
+  }
 }
