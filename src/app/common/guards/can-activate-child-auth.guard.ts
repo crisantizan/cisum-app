548a403ab579343620e175982f12dfd9dc7ed20f
Cristian Santiz <crisantizan@gmail.com>
2020-05-04 17:33:19 -0500

    feat: autologin from any available route

--- src/app/common/guards/can-activate-child-auth.guard.ts
+++ src/app/common/guards/can-activate-child-auth.guard.ts
@@ -6,14 +6,15 @@ import {
   UrlTree,
   Router,
 } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CanActivateChildAuthGuard implements CanActivateChild {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private _authService: AuthService, private _router: Router) {}
   canActivateChild(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -22,11 +23,32 @@ export class CanActivateChildAuthGuard implements CanActivateChild {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    if (this.authService.userIsLogged) {
+    // continue
+    if (this._authService.userIsLogged) {
       return true;
     }
 
-    this.router.navigate(['/login']);
+    // if a local token exists
+    if (this._authService.hasLocalToken()) {
+      // get data by token
+      return this._authService.whoami().pipe(
+        map(({ response: user }) => {
+          // set user
+          this._authService.setUser(user);
+          // and navigate to home page
+          return true;
+        }),
+        catchError((err) => {
+          console.log(err);
+          // error has ocurred, redirect to login page and clear token
+          this._authService.setToken(null);
+          this._router.navigate(['/login']);
+          return of(false);
+        })
+      );
+    }
+
+    this._router.navigate(['/login']);
     return false;
   }
 }
