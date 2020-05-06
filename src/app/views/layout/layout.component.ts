2bf6b63e6403d9f2e9504627901b281fea60de80
Cristian Santiz <crisantizan@gmail.com>
2020-05-06 12:51:43 -0500

    chore: test commit 2

--- src/app/views/layout/layout.component.ts
+++ src/app/views/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
@@ -6,20 +6,6 @@ import { SharedService } from 'src/app/services/shared.service';
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent implements OnInit {
-  // public loading: boolean;
-
+export class LayoutComponent {
   constructor(public sharedService: SharedService) {}
-
-  ngOnInit(): void {
-    // setTimeout(() => {
-    //   this.sharedService.loading$.subscribe((val) => {
-    //     console.log('STATE: ', val);
-    //     this.loading = val;
-    //   });
-    // }, 0);
-    // this.sharedService.loading.subscribe((value) => {
-    //   this.loading = value;
-    // });
-  }
 }
